34fd6264a00c81b6655fde3df98834930965912a
Gabriel Tavares <gabrielc.tavares02@gmail.com>
2023-10-09 10:06:33 -0300

    First test version.

--- hash_table.hpp
+++ hash_table.hpp
@@ -10,14 +10,12 @@
 #include <type_traits>
 
 namespace ht {
-	constexpr double DEFAULT_MAX_LOAD_FACTOR = 0.8;
-	constexpr double DEFAULT_MIN_LOAD_FACTOR = DEFAULT_MAX_LOAD_FACTOR / 4;
-
-	constexpr size_t DEFAULT_INITIAL_CPTY = 3;
-
-	// Smallest prime number greater than the size of the 
-	// input alphabet (considering all UTF-8 character codes)
-	constexpr size_t PRIME_GT_ALPHABET_SIZE = 257;
+	// Default upper limit for the HashTable load factor 
+	constexpr double MAX_LOAD_FACTOR = 0.8;
+	// Default lower limit for the HashTable load factor 
+	constexpr double MIN_LOAD_FACTOR = MAX_LOAD_FACTOR / 4;
+	// Minimum capacity allowed for the HashTable
+	constexpr size_t MIN_CPTY = 2;
 
 	template<typename Key, typename Value>
 	class HashTable {
@@ -29,133 +27,90 @@ namespace ht {
 		// Hash function interface
 		using HashFunction = std::function<size_t(const Key&)>;
 
-		// Default constructor
-		HashTable(HashFunction _customHashFn = nullptr) :
-			size(0),
-			capacity(DEFAULT_INITIAL_CPTY),
-			maxLoadFactor(DEFAULT_MAX_LOAD_FACTOR),
-			minLoadFactor(DEFAULT_MIN_LOAD_FACTOR),
-			map(DEFAULT_INITIAL_CPTY, nullptr),
-			customHashFn(_customHashFn)
-		{
-			if (customHashFn == nullptr) {
-				if (!(std::is_fundamental_v<Key> || std::is_pointer_v<Key> || std::is_array_v<Key>)) {
+		HashTable(HashFunction customFn = nullptr, size_t initialCapacity = MIN_CPTY, double minLoadFactor = MIN_LOAD_FACTOR, double maxLoadFactor = MAX_LOAD_FACTOR)
+			: _size(0), _customHashFn(customFn), _capacity(initialCapacity), _minLoadFactor(minLoadFactor), _maxLoadFactor(maxLoadFactor) {
+			if (_capacity < MIN_CPTY) 
+				throw std::invalid_argument("Invalid initial capacity.");
+			if (_customHashFn == nullptr)
+				if (!(std::is_fundamental_v<Key> || std::is_pointer_v<Key> || std::is_array_v<Key>))
 					throw std::invalid_argument("The default hash function accepts only contiguously allocated keys.");
-				}
-			}
+			map.resize(initialCapacity);
 		}
-		// Constructor with user-defined load factors
-		HashTable(double _maxLoadFactor, double _minLoadFactor, HashFunction _customHashFn = nullptr) :
-			size(0),
-			capacity(DEFAULT_INITIAL_CPTY),
-			maxLoadFactor(_maxLoadFactor),
-			minLoadFactor(_minLoadFactor),
-			map(DEFAULT_INITIAL_CPTY, nullptr),
-			customHashFn(_customHashFn)
-		{
-			if (customHashFn == nullptr) {
-				if (!(std::is_fundamental_v<Key> || std::is_pointer_v<Key> || std::is_array_v<Key>)) {
-					throw std::invalid_argument("The default hash function accepts only contiguously allocated keys.");
-				}
-			}
-		}
-		// Constructor with user-defined load factors and capacity
-		HashTable(double _maxLoadFactor, double _minLoadFactor, size_t _capacity, HashFunction _customHashFn = nullptr) :
-			size(0),
-			capacity(_capacity),
-			maxLoadFactor(_maxLoadFactor),
-			minLoadFactor(_minLoadFactor),
-			map(_capacity, nullptr),
-			customHashFn(_customHashFn)
-		{
-			if (capacity == 0) {
-				throw std::invalid_argument("The hash table capacity must not be 0");
-			}
-			if (customHashFn == nullptr) {
-				if (!(std::is_fundamental_v<Key> || std::is_pointer_v<Key> || std::is_array_v<Key>)) {
-					// If there is no custom hash function defined and 'Key' is not contiguously allocated,
-					// an exception is thrown
-					throw std::invalid_argument("The default hash function accepts only contiguously allocated keys.");
-				}
-			}
-		}
+
 		// Copy constructor
-		HashTable(const HashTable<Key, Value>& obj) :
-			size(obj.size),
-			capacity(obj.capacity),
-			maxLoadFactor(obj.maxLoadFactor),
-			minLoadFactor(obj.minLoadFactor),
-			map(obj.map.begin(), obj.map.end()),
-			customHashFn(obj.customHashFn)
-		{}
+		HashTable(const HashTable<Key, Value>& obj) 
+			: _size(obj._size), _customHashFn(obj._customHashFn), _capacity(obj._capacity), _minLoadFactor(obj._minLoadFactor), _maxLoadFactor(obj._maxLoadFactor), map(obj.map.begin(), obj.map.end()) {}
 
 		// Set upper and lower thresholds to the load factor
 		void setLoadFactors(double max, double min) {
 			if (min <= 0.0 || min > 1.0 || max <= 0.0 || max > 1.0 || max <= min) {
-				throw std::invalid_argument("Invalid load factors");
+				throw std::invalid_argument("Invalid load factors.");
 			}
-			minLoadFactor = min;
-			maxLoadFactor = max;
+			_minLoadFactor = min;
+			_maxLoadFactor = max;
 
 			if (maxLoadFactorExceeded()) {
-				rehash(2 * capacity);
+				rehash(2 * _capacity);
 			}
 			else if (minLoadFactorExceeded()) {
-				rehash((size_t)ceil(capacity / 2.0));
+				rehash((size_t)ceil(_capacity / 2.0));
 			}
 		}
+
 		// Get upper limit defined for hash table load factor 
 		double getMaxLoadFactor() const {
-			return maxLoadFactor;
+			return _maxLoadFactor;
 		}
+
 		// Get lower limit defined for hash table load factor 
 		double getMinLoadFactor() const {
-			return minLoadFactor;
+			return _minLoadFactor;
 		}
 
 		// Update map capacity with newCapacity and rehash
 		void rehash(size_t newCapacity) {
-			if (newCapacity < size) {
-				throw std::invalid_argument("The map capacity shouldn't be smaller than its size.");
+			if (newCapacity < _size) {
+				throw std::invalid_argument("The HashTable capacity shouldn't be smaller than its size.");
 			}
-			size_t oldCapacity = capacity;
-			capacity = newCapacity;
+			size_t oldCapacity = _capacity;
+			_capacity = newCapacity;
 
 			Map_t temp;
-			temp.resize(capacity);
+			temp.resize(_capacity);
 
 			// Transfer non-empty slots from the current map to temp
 			for (size_t i = 0; i < oldCapacity; i++) {
 				if (map[i]) {
-					size_t h = hash(map[i]->first, capacity);
+					size_t h = hash(map[i]->first, _capacity);
 
 					if (!temp[h]) {
 						temp[h] = map[i];
+						continue;
 					}
-					else {
-						size_t h2 = (h + 1) % capacity;
+					// A collision has occurred. Try to insert in another place
+					// using linear probing
+					size_t h2 = (h + 1) % _capacity;
 
-						do {
-							if (!temp[h2]) {
-								temp[h2] = map[i];
-								break;
-							}
-							h2 = (h2 + 1) % capacity;
-						} while (h2 != h);
+					do {
+						if (!temp[h2]) {
+							temp[h2] = map[i];
+							break;
+						}
+						h2 = (h2 + 1) % _capacity;
+					} while (h2 != h);
 
-						if (h2 == h)
-							throw std::exception("Error rehashing HashTable.");
-					}
+					if (h2 == h)
+						throw std::exception("Error: HashTable could not be resized.");
 				}
 			}
 			// Swap the contents of the current map with the temporary map
 			map.swap(temp);
 		}
 
-		// Search for value paired with key 'k'
-		// If found, returns it; otherwise, returns a std::nullopt
+		// Search for item with key 'k' in the HashTable
+		// If found, its value is returned; otherwise, a std::nullopt is returned
 		std::optional<Value> search(const Key& k) const {
-			size_t h = hash(k, capacity);
+			size_t h = hash(k, _capacity);
 
 			if (!map[h]) {
 				return std::nullopt; // Key not found
@@ -165,35 +120,35 @@ namespace ht {
 				std::optional<Value> r = map[h]->second;
 				return r;
 			}
-			size_t h2 = (h + 1) % capacity;
+			// Probe through HashTable searching for the required item
+			size_t h2 = (h + 1) % _capacity;
 
 			do {
 				if (!map[h2]) {
-					return std::nullopt; // Key not found
+					return std::nullopt;
 				}
 				if (map[h2]->first == k) {
-					// Return slot value wrapped on a std::optional class object
 					std::optional<Value> r = map[h2]->second;
 					return r;
 				}
-				h2 = (h2 + 1) % capacity;
+				h2 = (h2 + 1) % _capacity;
 			} while (h2 != h);
 
 			return std::nullopt;
 		}
 
-		// Insert new (key, value) pair in the hash table
+		// Insert new item in the HashTable
 		// Returns true if insertion was effectively done; otherwise, returns false
 		bool insert(const Key& k, const Value& v) {
-			size_t h = hash(k, capacity);
+			size_t h = hash(k, _capacity);
 
 			if (!map[h]) {
 				// Insert new pair
 				map[h] = std::make_shared<std::pair<Key, Value>>(k, v);
-				size++;
+				_size++;
 				// Check if load factor exceeded its limit and rehash if necessary
 				if (maxLoadFactorExceeded()) {
-					rehash(2 * capacity);
+					rehash(2 * _capacity);
 				}
 				return true; // Insertion done successfully
 			}
@@ -201,33 +156,32 @@ namespace ht {
 				// Don't insert duplicates
 				return false;
 
-			size_t h2 = (h + 1) % capacity;
+			// A collision has occurred. Try to insert in another place
+			// using linear probing
+			size_t h2 = (h + 1) % _capacity;
 
 			do {
 				if (!map[h2]) {
-					// Insert new pair
 					map[h2] = std::make_shared<std::pair<Key, Value>>(k, v);
-					size++;
-					// Check if load factor exceeded its limit and rehash if necessary
+					_size++;
 					if (maxLoadFactorExceeded()) {
-						rehash(2 * capacity);
+						rehash(2 * _capacity);
 					}
-					return true; // Insertion done successfully
+					return true; 
 				}
 				if (map[h2]->first == k) {
-					// Don't insert duplicates
 					return false;
 				}
-				h2 = (h2 + 1) % capacity;
+				h2 = (h2 + 1) % _capacity;
 			} while (h2 != h);
 
-			throw std::exception("Error inserting value into HashTable.");
+			throw std::exception("Error: item could not be inserted in HashTable.");
 		}
 
-		// Search for slot with key 'k' in the hash table and, if found, 
-		// remove it and returns its value; otherwise, returns a std::nullopt
+		// Search for slot with key 'k' in the HashTable and, if found, remove it 
+		// and returns its value; otherwise, returns a std::nullopt
 		std::optional<Value> remove(const Key& k) {
-			size_t h = hash(k, capacity);
+			size_t h = hash(k, _capacity);
 
 			if (!map[h]) {
 				return std::nullopt; // Key not found
@@ -237,37 +191,36 @@ namespace ht {
 				std::optional<Value> r = map[h]->second;
 				// Empty slot
 				map[h].reset();
-				size--;
+				_size--;
 				// Check if load factor exceeded its limit and rehash if necessary
-				if (minLoadFactorExceeded() && capacity > 1) {
-					rehash((size_t)ceil(capacity / 2.0));
+				if (minLoadFactorExceeded() && _capacity > 1) {
+					rehash((size_t)ceil(_capacity / 2.0));
 				}
 				return r; // Return removed slot value
 			}
-			size_t h2 = (h + 1) & capacity;
+			// Probe through HashTable searching for the required item
+			size_t h2 = (h + 1) & _capacity;
 
 			do {
 				if (!map[h2]) {
-					return std::nullopt; // Key not found
+					return std::nullopt;
 				}
 				if (map[h2]->first == k) {
-					// Return value wrapped on a std::optional class object
 					std::optional<Value> r = map[h2]->second;
-					// Empty slot
 					map[h2].reset();
-					size--;
-					// Check if load factor exceeded its limit and rehash if necessary
-					if (minLoadFactorExceeded() && capacity > 1) {
-						rehash((size_t)ceil(capacity / 2.0));
+					_size--;
+					if (minLoadFactorExceeded() && _capacity > 1) {
+						rehash((size_t)ceil(_capacity / 2.0));
 					}
-					return r; // Return removed slot value
+					return r; 
 				}
-				h2 = (h2 + 1) % capacity;
+				h2 = (h2 + 1) % _capacity;
 			} while (h2 != h);
 
 			return std::nullopt;
 		}
 
+		// Get a vector with the values of all items stored in the HashTable
 		std::vector<Value> getAll() const {
 			std::vector<Value> result;
 
@@ -280,24 +233,24 @@ namespace ht {
 
 		// Check if the hash table is empty
 		bool isEmpty() const {
-			return size == 0;
+			return _size == 0;
 		}
 
 	private:
 		Map_t map;
 
 		// Total number of slots
-		size_t capacity;
+		size_t _capacity;
 		// Number of occupied slots
-		size_t size;
+		size_t _size;
 
 		// Maximum size/capacity value
-		double maxLoadFactor;
+		double _maxLoadFactor;
 		// Minimum size/capacity value
-		double minLoadFactor;
+		double _minLoadFactor;
 
 		// Custom hash function
-		HashFunction customHashFn;
+		HashFunction _customHashFn;
 
 		// Check if 'n' is a prime number
 		bool isPrime(size_t n) const {
@@ -312,9 +265,10 @@ namespace ht {
 		}
 
 		// Calculate the hash code of the key 'k'
+		// The retuned value will be in the range [0, range - 1]
 		size_t hash(const Key& k, size_t range) const {
 			// Use the custom hash function if provided; otherwise, use the default hash function
-			auto hashFn = customHashFn ? customHashFn : [this, range](const Key& k) -> size_t {
+			auto hashFn = _customHashFn ? _customHashFn : [this, range](const Key& k) -> size_t {
 				// 'ptr' points to the memory representation of 'k'
 				const char* ptr = nullptr;
 				ptr = reinterpret_cast<const char*>(&k);
@@ -322,14 +276,19 @@ namespace ht {
 				if (!ptr) {
 					throw std::runtime_error("Invalid key type for default hash function.");
 				}
+				// Assuming the input alphabet can contain all UTF-8 characters
+				static const uint16_t alphabetSize = UCHAR_MAX + 1;
+				// Some prime number greater than the input alphabet size
+				static const uint16_t somePrime = alphabetSize + 1;
+
 				const size_t blockSize = sizeof(k);
-				size_t primePow = PRIME_GT_INPUT_ALPH;
+				size_t primePow = somePrime;
 				size_t hashCode = 0;
 
 				// Treat 'k' as a byte string and apply polynomial rolling hash
 				for (size_t i = 0; i < blockSize; i++) {
 					hashCode = (hashCode + (static_cast<size_t>(*(ptr + i)) + 1) * primePow) % range;
-					primePow = (primePow * PRIME_GT_INPUT_ALPH) % range;
+					primePow = (primePow * somePrime) % range;
 				}
 				return hashCode;
 				};
@@ -339,11 +298,11 @@ namespace ht {
 
 		// Check if the load factor has reached its maximum value
 		bool maxLoadFactorExceeded() const {
-			return (static_cast<double>(size) / capacity) >= maxLoadFactor;
+			return (static_cast<double>(_size) / _capacity) > _maxLoadFactor;
 		}
 		// Check if the load factor has reached its minimum value
 		bool minLoadFactorExceeded() const {
-			return (static_cast<double>(size) / capacity) < minLoadFactor;
+			return (static_cast<double>(_size) / _capacity) < _minLoadFactor;
 		}
 	};
 }
